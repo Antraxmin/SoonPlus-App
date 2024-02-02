fdd1b130801cb71da9e8429759930c7c16df3efc
Chaemin-Lim <antraxmin@naver.com>
2024-02-02 15:54:55 +0900

    feat: 학내순환,셔틀버스,지하철 탭 전환 구현

--- lib/screen/transport_page.dart
+++ lib/screen/transport_page.dart
@@ -5,20 +5,52 @@ class TransportPage extends StatefulWidget {
   _TransportPageState createState() => _TransportPageState();
 }
 
-class _TransportPageState extends State<TransportPage> {
+class _TransportPageState extends State<TransportPage>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 3, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            '셔틀버스/지하철',
-            style: TextStyle(fontSize: 17),
-          ),
-          backgroundColor: Colors.blue[900],
-          foregroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          '셔틀버스/지하철',
+          style: TextStyle(fontSize: 17),
         ),
-        body: const Text('셔틀버스/지하철 및 기타 교통수단 정보 페이지입니다')
+        bottom: TabBar(
+          controller: controller,
+          indicatorColor: Colors.yellow,
+          labelColor: Colors.white,
+          unselectedLabelColor: Colors.white,
+          tabs: const <Widget>[
+            Tab(text: "셔틀버스"),
+            Tab(text: "통학버스"),
+            Tab(text: "지하철")
+          ],
+        ),
+        backgroundColor: Colors.blue[900],
+        foregroundColor: Colors.white,
+      ),
+      body: TabBarView(
+        controller: controller,
+        children: const <Widget>[
+          Center(
+            child: Text("셔틀버스 페이지입니다")
+          ),
+          Center(
+              child: Text("통학버스 페이지입니다")
+          ),
+          Center(
+              child: Text("지하철 페이지입니다")
+          )
+        ],
+      )
     );
   }
 }
-
